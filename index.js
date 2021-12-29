c37089222b5916cf7269a9de1ee78ed6652c6d95
cartwheee11 <cartwheel@MBP-Cartwheel.Dlink>
2021-12-29 21:36:46 +0300

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+let ws = require('ws');
+let config = require('./config.json');
+let pass = config.pass;
+
+let server = new ws.Server({ port: 5051 })
+
+server.on('connection', function(connection) {
+  console.log('соединение состоялось');
+  connection.on('message', function(message) {
+    console.log('пришло сообщение:')
+    message = JSON.parse(message)
+    console.log(message);
+
+    if(message.type == 'pass') {
+      if(message.data == pass) {
+        let query = { type: 'verify', data: { result: "success", gift: '98965739715632' } }
+        connection.send(JSON.stringify(query));
+      } else {
+        let query = { type: 'verify', data: { result: "fail" } }
+        connection.send(JSON.stringify(query));
+      }
+    }
+  });
+})
+
+server.on('close', function(err) {
+  console.log('соединение разорвано');
+})
